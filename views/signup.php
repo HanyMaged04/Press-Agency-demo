59f96a4af741ff088c909cb89ff23f90116da081
Hany_Maged <Hany.Maged9102@gmail.com>
2023-11-26 12:18:22 +0200

    .

---
+++ views/signup.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SignUp</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+</head>
+<body>
+    <script src="../assets/js/popper.min.js"></script>
+    <script src="../assets/js/jquery-3.7.1.min.js"></script>
+    <script src="../assets/js/bootstrap.js"></script>
+    <?php include('../assets/navBar/navBar.php') ?>
+    
+    <?php 
+    setcookie("name","hany",time() +((24*60*60)*2)*0,'/');
+
+    ?>
+</body>
+</html>
