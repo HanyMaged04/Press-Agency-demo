6a0d1e4ae972978926452c204bd0e1585304934b
Hany_Maged <Hany.Maged9102@gmail.com>
2023-11-26 12:25:08 +0200

    .

--- assets/databaseSchema.sql
+++ assets/databaseSchema.sql
@@ -1,54 +1,62 @@
-create database TestProjectPhp;
-use TestProjectPhp;
-create schema userInfo;
+CREATE DATABASE TestProjectPhp;
+USE TestProjectPhp;
 
+CREATE SCHEMA userInfo;
 
-create table userInfo.types(
-    id int primary key,
-    typeName varchar(255)
+CREATE TABLE userInfo.types (
+    id INT PRIMARY KEY,
+    typeName VARCHAR(255)
+);
+
+CREATE TABLE userInfo.users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fname VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL,
+    lname VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL,
+    phoneNum VARCHAR(11) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    userType INT,
+    password VARCHAR(255) NOT NULL,
+    urlToPhoto VARCHAR(255) DEFAULT NULL,
+    CONSTRAINT UniqueName UNIQUE (fname, lname),
+    FOREIGN KEY (userType) REFERENCES userInfo.types(id)
 );
 
 
-create table userInfo.users (
-    id int AUTO_INCREMENT primary key,
-    fname varchar(255) CHARACTER SET utf8mb4 not null,
-    lname varchar(255) CHARACTER SET utf8mb4 not null,
-    phoneNum varchar(11) not null,
-    email varchar(255) not null unique ,
-    userType int foreign key references userInfo.types(id),
-    password varchar(255) not null,
-    urlToPhoto varchar(255) default NULL,
-    constraint UniqueName unique (fname, lname)
+
+CREATE SCHEMA posts;
+
+CREATE TABLE posts.postTypes (
+    id INT PRIMARY KEY,
+    typeName VARCHAR(255)
 );
 
-
-
-create schema posts;
-create table posts.postTypes(
-    id int primary key,
-    typeName varchar(255)
-);
-create table posts.post(
-    postId int AUTO_INCREMENT primary key,
-    title varchar(255) CHARACTER SET utf8mb4,
-    body varchar(1024) CHARACTER SET utf8mb4,
-    creationTime  DATE DEFAULT CURRENT_DATE,
-    postType int foreign key references posts.post(id);
-    numViews int default(0),
-    urlToPhoto varchar(255) default NULL,
-    likesNum int default(0),
-    commentNum int default(0),
-    ownerId int foreign key references users(id),
+CREATE TABLE posts.post (
+    postId INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) CHARACTER SET utf8mb4,
+    body VARCHAR(1024) CHARACTER SET utf8mb4,
+    creationTime DATE DEFAULT CURRENT_DATE,
+    postType INT,
+    numViews INT DEFAULT 0,
+    urlToPhoto VARCHAR(255) DEFAULT NULL,
+    likesNum INT DEFAULT 0,
+    commentNum INT DEFAULT 0,
+    ownerId INT,
+    FOREIGN KEY (postType) REFERENCES posts.postTypes(id),
+    FOREIGN KEY (ownerId) REFERENCES userInfo.users(id)
 );
 
-create table posts.comments(
-    postId int foreign key references posts.post(postId),
-    userId int foreign key references users(id),
-    comment varchar(1024) not null,
+CREATE TABLE posts.comments (
+    postId INT,
+    userId INT,
+    comment VARCHAR(1024) NOT NULL,
+    FOREIGN KEY (postId) REFERENCES posts.post(postId),
+    FOREIGN KEY (userId) REFERENCES userInfo.users(id)
 );
 
-create table posts.savedPost(
-    postId int foreign key references posts.post(postId),
-    userId int foreign key references users(id)
-    primary key (userId,postId)
+CREATE TABLE posts.savedPost (
+    postId INT,
+    userId INT,
+    FOREIGN KEY (postId) REFERENCES posts.post(postId),
+    FOREIGN KEY (userId) REFERENCES userInfo.users(id),
+    PRIMARY KEY (userId, postId)
 );
